88aec42f74d3574d4c3744eb4dfdea20d865386e
Zach R <zachkery999@gmail.com>
2020-08-31 15:41:06 -0500

    added detail template and http404 exception

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import Http404
-from django.http import HttpResponse
+
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
